f9795108ae8045685b9a1b1c2761e56b83a51eab
Daniele Servadei <servadei.d@gmail.com>
2020-09-10 22:00:00 +0200

    init elastic-beanstalk-hello-world

---
+++ README.md
@@ -0,0 +1,3 @@
+# elastic-beanstalk-hello-world
+
+Hello World NodeJS application on S3 to use as default project for AWS Elastic Beanstalk Terraform configuration.
---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require("express")
+const app = express()
+const port = process.env.PORT || 3000
+
+app.get('/', (req, res) => {
+  res.send("Hello World, from Sellix.")
+})
+
+app.listen(port, () => {
+  console.log(`Hello Sellix listening at http://localhost:${port}`)
+})
