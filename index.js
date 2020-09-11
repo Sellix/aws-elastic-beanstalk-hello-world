c408641bc62d3e0a726b6c1643852890174a621e
Daniele Servadei <servadei.d@gmail.com>
2020-09-11 08:53:23 +0200

    add NodeJS version and update PORT name

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express")
 const app = express()
-const port = process.env.PORT || 3000
+const port = process.env.ELASTIC_BEANSTALK_PORT || 3000
 
 app.get('/', (req, res) => {
   res.send("Hello World, from Sellix.")
